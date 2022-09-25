23907d6686b58f2209456d5bfc836f6aa884243f
Damiosoka <112974963+Damiosoka@users.noreply.github.com>
2022-09-25 10:56:14 +0200

    sunday_dami_commit_2

--- 1-What_is_Python.py
+++ 1-What_is_Python.py
@@ -6,9 +6,11 @@
 # Cross-Platform
 # Huge Community
 # Large Ecosystem
+# Readability
 
 # What is programming Languages? 
-# they are series of symbols that serves as bridge that allows human to translate tgheir thoughts into the language the computer understands
+# they are series of symbols, that serves as bridge that allows human to translate their 
+# thoughts into the lan<guage the computer understands 
 # programming languages are divided into two:
 # Low level(close to the machine code and not easy to read by humans but faster to operate
 # High Level(Close to human daily languages)
@@ -16,3 +18,6 @@
 # Python, Java, Java script, C#(microsoft), C++ and so on
 # Syntax; the well-formed structure of statements in a computer language.
 # Python basics?
+# Variables are containers for storing data values.
+# What is a comment?
+# They are used to expalin Python code and also prevent execution
